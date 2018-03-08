7aee407e4b4b00bce21f10e48159fc0ade63470d
Yoon Han Sol <yhs1514@hanmail.net>
2018-03-08 14:58:15 +0900

    Function(checkIsOrNot) is added in dankookFunction.py

--- dankookFunction.py
+++ dankookFunction.py
@@ -19,3 +19,22 @@ def getHouseInfoC(driver):
         m=e.find_element_by_xpath('.//div[2]/a/div[2]')
         str=str+m.text+"\n"
     print(str)
+
+def checkIsOrNot(driver,beforeDate):
+    import datetime
+
+    check=False
+    now=datetime.datetime.now()
+    nowDate=now.strftime('%m.%d')
+
+    driver.get('http://www.dankook.ac.kr/web/kor/-390')
+    driver.implicitly_wait(5)
+
+    elements=driver.find_elements_by_xpath('//*[@id="p_p_id_Bbs_WAR_bbsportlet_"]/div/div/div/div[2]/div[2]/ul/li')
+    for e in elements:
+        dateStr=e.find_element_by_class_name("table_date").text
+        eDate=dateStr[5:]
+        if(beforeDate<=eDate and eDate<=nowDate):
+            check=True
+    
+    print(check)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     driver=webdriver.Chrome('C:/Users/root/Documents/hansol/workspace/portalChecker/chromedriver.exe')
     df.getHouseInfo(driver)
     df.getHouseInfoC(driver)
+    df.checkIsOrNot(driver,'03.06')
 
     driver.quit()
 
