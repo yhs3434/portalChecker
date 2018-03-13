441616bf43e42366f48fdfcc119244680a380604
Yoon Han Sol <yhs1514@hanmail.net>
2018-03-13 13:59:13 +0900

    pre complete

--- dankookFunction.py
+++ dankookFunction.py
@@ -2,10 +2,19 @@ from openpyxl import Workbook
 import openpyxl
 import datetime
 import numpy as np
+from url import getURL
 
 class portalChecker:
-    def __init__(self,driver):
+    def __init__(self,driver,id,pw):
         self.driver=driver
+        self.driver.get('https://portal.dankook.ac.kr/web/portal')
+        self.driver.implicitly_wait(10)
+        print(driver.get_window_size)
+        idElement=self.driver.find_element_by_xpath('//*[@id="username"]')
+        pwElement=self.driver.find_element_by_xpath('//*[@id="password"]')
+        idElement.send_keys(id)
+        pwElement.send_keys(pw)
+        pwElement.submit()
 
     def getHouseInfo(self):
         self.driver.get('https://portal.dankook.ac.kr/web/portal/-13')
@@ -32,21 +41,32 @@ class portalChecker:
         excel_doc=openpyxl.load_workbook('portalCheck.xlsx')
         sheet=excel_doc.active
 
-        check=False
         now=datetime.datetime.now()
-        nowDate=now.strftime('%m.%d')
+        nowDate=now.strftime('%Y.%m.%d')
 
-        self.driver.get('http://www.dankook.ac.kr/web/kor/-390')
-        self.driver.implicitly_wait(5)
-
-        elements=self.driver.find_elements_by_xpath('//*[@id="p_p_id_Bbs_WAR_bbsportlet_"]/div/div/div/div[2]/div[2]/ul/li')
-        for e in elements:
-            dateStr=e.find_element_by_class_name("table_date").text
-            eDate=dateStr[5:]
-            if(beforeDate<=eDate and eDate<=nowDate):
-                check=True
+        monthDay=now.strftime('%m-%d')
+        sheet.cell(row=4,column=4).value=monthDay
         
-        for i in np.arange(48):
-            self.driver.get()
+        timeBefore=datetime.datetime.strptime(beforeDate,'%Y.%m.%d')
+        timeCurrent=datetime.datetime.strptime(nowDate,'%Y.%m.%d')
+        
+        for i in np.arange(47):
+            check=False
+            self.driver.get(getURL(i))
+            self.driver.implicitly_wait(10)
             elements=self.driver.find_elements_by_xpath('//*[@class="table_date"]')
+            for e in elements:
+                contentDate=e.text
+                timeContent=datetime.datetime.strptime(contentDate,'%Y.%m.%d')
+                if(timeBefore<=timeContent and timeContent<=timeCurrent):
+                    check=True
             
+            if(check==True):
+                sheet.cell(row=i+5,column=4).value='o'
+            else:
+                sheet.cell(row=i+5,column=4).value='x'
+        
+        fileName='portalCheck-'
+        fileName+=now.strftime('%m_%d')
+        fileName+='.xlsx'
+        excel_doc.save(fileName)
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 import dankookFunction
 
 if __name__ == "__main__":
-    
+    id = input('ID : ')
+    pw = input('PW : ')
+
     driver=webdriver.Chrome('chromedriver.exe')
-    pc=dankookFunction.portalChecker(driver)
-    pc.getHouseInfo()
-    pc.getHouseInfoC()
-    pc.checkIsOrNot('03.06')
+    driver.set_window_size(1200,500)
+    pc=dankookFunction.portalChecker(driver,id,pw)
+
+    pc.checkIsOrNot('2018.03.12')
 
     driver.quit()
 
